bfd4ada0c79619e0a69a1592f0052c2fb65dbab8
HuangZhiMing <huangzhimingme@gmail.com>
2021-06-01 21:41:22 +0800

    init project

---
+++ src/main/java/pers/zhiming/springcafe/SpringCafeApplication.java
@@ -0,0 +1,13 @@
+package pers.zhiming.springcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCafeApplication.class, args);
+    }
+
+}
